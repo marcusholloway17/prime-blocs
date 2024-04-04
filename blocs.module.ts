98ed2ce1707c562d8685e1294b730f583fb19ba4
marcusholloway17 <loganjean17@gmail.com>
2024-04-04 16:38:09 +0000

    import SidebarModule & DividerModule

--- blocs.module.ts
+++ blocs.module.ts
@@ -1,30 +1,32 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LoaderComponent } from './loader/loader.component';
-import { ProgressSpinnerModule } from 'primeng/progressspinner';
-import { BlockUIModule } from 'primeng/blockui';
-import { CrudComponent } from './crud/crud.component';
-import { ToolbarModule } from 'primeng/toolbar';
-import { TableModule } from 'primeng/table';
-import { ButtonModule } from 'primeng/button';
-import { TranslateModule } from '@ngx-translate/core';
-import { InputTextModule } from 'primeng/inputtext';
-import { PaginatorModule } from 'primeng/paginator';
-import { DialogModule } from 'primeng/dialog';
-import { NgxSmartFormModule } from '@azlabsjs/ngx-smart-form';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RequestPipe } from './pipes/request.pipe';
-import { SafePipe } from './pipes/safe.pipe';
-import { WithLoadingPipe } from './pipes/with-loading.pipe';
-import { CalendarModule } from 'primeng/calendar';
-import { PasswordModule } from 'primeng/password';
-import { InputNumberModule } from 'primeng/inputnumber';
-import { RadioButtonModule } from 'primeng/radiobutton';
-import { PrimeFormControlComponent } from './prime-form-control/prime-form-control.component';
-import { EditorModule } from 'primeng/editor';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { InputMaskModule } from 'primeng/inputmask';
-import { DropdownModule } from 'primeng/dropdown';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { LoaderComponent } from "./loader/loader.component";
+import { ProgressSpinnerModule } from "primeng/progressspinner";
+import { BlockUIModule } from "primeng/blockui";
+import { CrudComponent } from "./crud/crud.component";
+import { ToolbarModule } from "primeng/toolbar";
+import { TableModule } from "primeng/table";
+import { ButtonModule } from "primeng/button";
+import { TranslateModule } from "@ngx-translate/core";
+import { InputTextModule } from "primeng/inputtext";
+import { PaginatorModule } from "primeng/paginator";
+import { DialogModule } from "primeng/dialog";
+import { NgxSmartFormModule } from "@azlabsjs/ngx-smart-form";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { RequestPipe } from "./pipes/request.pipe";
+import { SafePipe } from "./pipes/safe.pipe";
+import { WithLoadingPipe } from "./pipes/with-loading.pipe";
+import { CalendarModule } from "primeng/calendar";
+import { PasswordModule } from "primeng/password";
+import { InputNumberModule } from "primeng/inputnumber";
+import { RadioButtonModule } from "primeng/radiobutton";
+import { PrimeFormControlComponent } from "./prime-form-control/prime-form-control.component";
+import { EditorModule } from "primeng/editor";
+import { InputTextareaModule } from "primeng/inputtextarea";
+import { InputMaskModule } from "primeng/inputmask";
+import { DropdownModule } from "primeng/dropdown";
+import { SidebarModule } from "primeng/sidebar";
+import { DividerModule } from "primeng/divider";
 
 @NgModule({
   declarations: [
@@ -57,6 +59,8 @@ import { DropdownModule } from 'primeng/dropdown';
     InputTextareaModule,
     InputMaskModule,
     DropdownModule,
+    SidebarModule,
+    DividerModule,
   ],
   exports: [
     LoaderComponent,
