1add9f1756c10f2ff1c5f95a2a019603ff24dc38
marcusholloway17 <loganjean17@gmail.com>
2024-04-08 16:37:42 +0000

    stringify params options

--- pipes/request.pipe.ts
+++ pipes/request.pipe.ts
@@ -1,9 +1,9 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, map, take, throwError } from 'rxjs';
+import { Pipe, PipeTransform } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable, catchError, map, take, throwError } from "rxjs";
 
 @Pipe({
-  name: 'request',
+  name: "request",
 })
 export class RequestPipe implements PipeTransform {
   constructor(private http: HttpClient) {}
@@ -16,21 +16,30 @@ export class RequestPipe implements PipeTransform {
   ): Observable<any> {
     const apiUrl = `${url}${value}`;
 
-    return this.http.get(apiUrl, { params: params }).pipe(
-      catchError((err) => {
-        // handle error logic
-
-        return throwError(() => err);
-      }),
-      take(1),
-      map((data: any) => {
-        if (columns) {
-          const values = columns.map((column) => data[column]).join(' ');
-          return values;
-        } else {
-          return data;
-        }
+    return this.http
+      .get(apiUrl, {
+        params: !params
+          ? {}
+          : {
+              ...params,
+              _query: params?._query ? JSON.stringify(params._query) : {},
+            },
       })
-    );
+      .pipe(
+        catchError((err) => {
+          // handle error logic
+
+          return throwError(() => err);
+        }),
+        take(1),
+        map((data: any) => {
+          if (columns) {
+            const values = columns.map((column) => data[column]).join(" ");
+            return values;
+          } else {
+            return data;
+          }
+        })
+      );
   }
 }
